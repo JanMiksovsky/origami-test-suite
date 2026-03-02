921fe03d8c2295d661a40030e8d029c609f5d899
Jan Miksovsky <jan@miksovsky.com>
2026-03-02 15:14:38 -0800

    Tweaks

--- failures-only.js
+++ failures-only.js
@@ -7,16 +7,14 @@ export default async function* reporter(source) {
       passed++;
     } else if (event.type === "test:fail") {
       failed++;
-      // const { name, details } = event.data;
-      // const message = details?.error?.message ?? details?.error ?? "";
-      // yield `FAIL: ${name}\n  ${message}\n\n`;
+      yield `failed: ${event.data.file}\n`;
     }
   }
 
-  const total = passed + failed;
-  if (failed > 0) {
-    yield `${failed} failed, ${passed} passed, ${total} total\n`;
-  } else {
-    yield `${total} tests passed\n`;
-  }
+  // const total = passed + failed;
+  // if (failed > 0) {
+  //   yield `${failed} failed, ${passed} passed, ${total} total\n`;
+  // } else {
+  //   yield `${total} tests passed\n`;
+  // }
 }
